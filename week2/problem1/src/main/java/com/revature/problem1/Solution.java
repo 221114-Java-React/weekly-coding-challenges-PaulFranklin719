c17b4b8dba83c5016b7dca8da5208634fcdbe8e3
Paul Franklin <paul219@revature.net>
2022-11-23 11:48:37 -0500

    create new

--- week2/problem1/src/main/java/com/revature/problem1/Solution.java
+++ week2/problem1/src/main/java/com/revature/problem1/Solution.java
@@ -3,6 +3,12 @@ package com.revature.problem1;
 public class Solution {
     public String reverseString(String s) {
         // feel free to comment this line out or delete it
-        throw new RuntimeException("Todo! Needs implementation...");
+//        throw new RuntimeException("Todo! Needs implementation...");
+        String a = "";
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            a = a + s.substring(i,i+1);
+        }
+        return a;
     }
 }
--- week2/problem2/src/main/java/com/revature/problem2/Solution.java
+++ week2/problem2/src/main/java/com/revature/problem2/Solution.java
@@ -6,6 +6,26 @@ import java.util.List;
 public class Solution {
     public List<String> abracadabra(int n) {
         /* You can comment or delete this line of code */
-        throw new RuntimeException("TODO! Needs implementation...");
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 1; i < n+1; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                strings.add("abracadabra");
+            }
+            else if (i % 3 == 0)
+            {
+                strings.add("abraca");
+            }
+            else if (i % 5 == 0)
+            {
+                strings.add("dabra");
+            }
+            else
+            {
+                strings.add(""+i+"");
+            }
+        }
+        return strings;
     }
 }
